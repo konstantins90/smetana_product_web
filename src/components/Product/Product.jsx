2c4c506fedab6d585872d1d8dbd39e585ed39d25
Konstantin Smetana <konstantinsmetana@gmail.com>
2024-09-06 00:34:51 +0200

    add button

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './Product.css';
 import banner from '../../assets/images/banner.jpg';
+import {Link} from "react-router-dom";
 
 const Product = () => {
     return (
@@ -18,6 +19,9 @@ const Product = () => {
                     </h1>
                 </div>
                 <p className='text-center'>Die CellPower Wasserstoff Wasserflasche von LUMIVITEA ist mehr als nur eine gewöhnliche Flasche. Sie nutzt modernste Elektrolisetechnologie, um Ihre Wasseraufnahme mit reinem Wasserstoff anzureichern. Dies verbessert nicht nur die Hydration, sondern unterstützt auch Ihre Gesundheit und Vitalität. Ideal für alle, die auf der Suche nach einem effektiven Weg sind, ihre Flüssigkeitszufuhr zu optimieren und gleichzeitig von den gesundheitsfördernden Eigenschaften von Wasserstoff zu profitieren.</p>
+            </div>
+            <div className='py-10'>
+                <Link to='/form' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Bestellen</Link>
             </div>
         </div>
     );
